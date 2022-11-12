7e6f2d7ef5ac05b7a2f9e103facc86b699142864
IreneQuero <queroirene@gmail.com>
2022-11-12 12:10:36 +0000

    ejercicio terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = generateDomain();
+};
+
+let generateDomain = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = [".com", ".io", ".es", ".net"];
+
+  let concatenation = "";
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let h = 0; h < extension.length; h++) {
+          concatenation +=
+            "<p>" + pronoun[i] + adj[j] + noun[k] + extension[h] + "</p>";
+        }
+      }
+    }
+  }
+  return concatenation;
 };
